8d05b073fef2fdb66bb4e3020ee410b3ce5cbab6
selango007 <85666764+selango007@users.noreply.github.com>
2021-08-20 14:16:15 -0700

    FLASK all routs added

--- app.py
+++ app.py
@@ -1,31 +1,91 @@
-# 1. Import Flask
-from flask import Flask
+
+# Import Flask
+from flask import Flask, jsonify
+
+#1. Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+
+#2. Setup Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
 
 # 2. Create an app
 app = Flask(__name__)
+print("example __name__ = %s", __name__)
 
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
 
-# 3. Define index route
+#3. Define welcome route
 @app.route("/")
 def index():
-    return "Hello, world!"
-
-# 4. Define the about route
-@app.route("/about")
-def about():
-    name = "Peleke"
-    location = "Tien Shan"
-
-    return f"My name is {name}, and I live in {location}."
-
-# 5. Define the contact route
-@app.route("/contact")
-def contact():
-    email = "peleke@example.com"
-
-    return f"Questions? Comments? Complaints? Shoot an email to {email}."
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
 
+#4. Define precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+#5. Define stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+#5. Define tobs route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+#5. Define summary route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+    results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+    
 # 6. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
